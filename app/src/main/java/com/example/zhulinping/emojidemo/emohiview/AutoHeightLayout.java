446ffb211e70600c663387bd01c6821de118ec88
unknown <zhulinping@corp.apus.com>
2017-09-07 16:18:34 +0800

    布局顶起问题

--- app/src/main/java/com/example/zhulinping/emojidemo/emohiview/AutoHeightLayout.java
+++ app/src/main/java/com/example/zhulinping/emojidemo/emohiview/AutoHeightLayout.java
@@ -1,6 +1,9 @@
 package com.example.zhulinping.emojidemo.emohiview;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.res.Configuration;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
@@ -17,7 +20,8 @@ public abstract  class AutoHeightLayout extends SoftKeyboardSizeWatchLayout impl
     private static final int ID_CHILD = R.id.id_autolayout;
     protected Context mContext;
     protected int mSoftKeyboardHeight;
-
+    protected int mMaxParentHeight;
+    protected boolean mConfigurationChangedFlag = false;
     public AutoHeightLayout(Context context, AttributeSet arrs) {
         super(context,arrs);
         mContext = context;
@@ -52,6 +56,48 @@ public abstract  class AutoHeightLayout extends SoftKeyboardSizeWatchLayout impl
         super.onFinishInflate();
         onSoftKeyboardHeightChanged(mSoftKeyboardHeight);
     }
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        if (mMaxParentHeight == 0) {
+            mMaxParentHeight = h;
+        }
+    }
+
+    public void updateMaxParentHeight(int maxParentHeight) {
+        this.mMaxParentHeight = maxParentHeight;
+        if(maxParentHeightChangeListener != null){
+            maxParentHeightChangeListener.onMaxParentHeightChange(maxParentHeight);
+        }
+    }
+
+    @Override
+    protected void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mConfigurationChangedFlag = true;
+        mScreenHeight = 0;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        if(mConfigurationChangedFlag){
+            mConfigurationChangedFlag = false;
+            Rect r = new Rect();
+            ((Activity) mContext).getWindow().getDecorView().getWindowVisibleDisplayFrame(r);
+            if (mScreenHeight == 0) {
+                mScreenHeight = r.bottom;
+            }
+            int mNowh = mScreenHeight - r.bottom;
+            mMaxParentHeight = mNowh;
+        }
+
+        if (mMaxParentHeight != 0) {
+            int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+            int expandSpec = MeasureSpec.makeMeasureSpec(mMaxParentHeight, heightMode);
+            super.onMeasure(widthMeasureSpec, expandSpec);
+            return;
+        }
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
 
     @Override
     public void onSoftPop(int height) {
@@ -66,5 +112,14 @@ public abstract  class AutoHeightLayout extends SoftKeyboardSizeWatchLayout impl
     @Override
     public void onSoftClose() {
 
+    }
+    private OnMaxParentHeightChangeListener maxParentHeightChangeListener;
+
+    public interface OnMaxParentHeightChangeListener {
+        void onMaxParentHeightChange(int height);
+    }
+
+    public void setOnMaxParentHeightChangeListener(OnMaxParentHeightChangeListener listener) {
+        this.maxParentHeightChangeListener = listener;
     }
 }
